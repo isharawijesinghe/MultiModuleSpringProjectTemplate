ea43e0cc9e7022edcadbd4a118613d9f2aff78b9
isharawijesinghe <ishara.d.wijesinghe@gmail.com>
2021-11-07 09:14:18 +0530

    Initial commit

---
+++ RootApp/src/main/java/com/ish/java/rootapp/RootAppApplication.java
@@ -0,0 +1,13 @@
+package com.ish.java.rootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RootAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RootAppApplication.class, args);
+    }
+
+}
---
+++ RootApp/src/test/java/com/ish/java/rootapp/RootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ish.java.rootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
